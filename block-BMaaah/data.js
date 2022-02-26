5e0e359f6a9cfcedf59121e96890b4342b4c9373
RachamallaSwathi <swathirachamalla77@gmail.com>
2022-02-26 11:38:01 +0530

    assignment

---
+++ block-BMaaah/data.js
@@ -0,0 +1,41 @@
+articles=[
+    {
+      title: 'fruit',
+      details: 'inception',
+      author: {
+        name: 'apple',
+        email: 'apple@com',
+        age: '23',
+      },
+      tags: ['eat', 'cook']
+    }
+    ,
+    {
+        title: 'bank',
+        details: 'Bank of india',
+        author: {
+          name: 'swathii',
+          email: 'swathi@com',
+          age: '53',
+        },
+        tags: ['currency', 'rupya'],
+      
+      
+      
+    },
+    
+    {
+        title: 'code',
+        details: 'it is thing to execute',
+        author: {
+          name: 'java',
+          email: 'java@com',
+          age: null,
+        },
+        tags: ['run', 'compile'],
+      
+      
+      
+    },
+    
+    ]
